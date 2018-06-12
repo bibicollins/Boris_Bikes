826e4c0487477a2c5d18661d121a87aa77d624aa
Layth <laythq@gmail.com>
2018-06-12 16:43:53 +0100

    Docking Station and Bike (class + spec)

---
+++ lib/docking_station.rb
@@ -0,0 +1,7 @@
+class DockingStation
+
+def release_bike
+    bike = "bike"
+end
+
+end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,8 @@
+require 'bike'
+
+  describe Bike do
+    bike1 = Bike.new
+    it "responds to working method" do
+    expect(bike1).to respond_to(:working?)
+  end
+end
